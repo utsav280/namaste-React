008ef375c46d750d6dfe6e912903d1898c2fe803
utsav280 <utsav28feb@gmail.com>
2025-01-18 12:22:07 +0530

    Add initial project setup with .gitignore, package.json, and refactor App.js to use functional components

--- App.js
+++ App.js
@@ -1,2 +1,20 @@
-const heading = React.createElement("h1", {}, "Namaste React!!!");
-ReactDOM.render(heading, document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement("h1", {}, "Namaste React!!!");
+// const rootElement = document.getElementById("root");
+// const root = ReactDOM.createRoot(rootElement);
+// root.render(heading);
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React!!!!</h1>
+  </div>
+);
+
+const Title = () => <h2>Sub Heading</h2>;
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<HeadingComponent />);
